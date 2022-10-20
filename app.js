931ba4462b0dede892116c59e49bf1f1eb1a02af
AngelRose <glorianeangelmae@gmail.com>
2022-10-20 14:55:16 +0800

    Add function

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-const write = require('./write');
-const cmd = process.argv[2]
-//write(cmd);
+const add = require('./add')
+const read = require('./read')
+const present = require('./present')
 
-const read = require ('./read');
-console.log(read());
+
+//get user input
+const cmd = process.argv
+
+var note ={}
+if (cmd[2] == 'add'){
+    //Build Object
+    note = {
+        id: cmd[3],
+        title: cmd[4],
+        body: cmd[5]
+    }
+    //get old note value
+    var oldNote = read()
+    //add note to note
+    add(note,oldNote)
+}
+if (cmd[2] == 'read') {
+    present(read())
+}
